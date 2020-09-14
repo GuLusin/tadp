4c6353a9dbee0f3e403972f38fa2ae29265df12a
GuLusin <guido.lusin@gmail.com>
2020-09-13 22:52:17 -0300

    muevo lo agregado a un archivo separado

--- ruby/lib/main.rb
+++ ruby/lib/main.rb
@@ -1,21 +1,4 @@
-#esto va en un archivo aparte pero nos falla ...
-def before_and_after_each_call(pre_cond, post_cond) #TODO
-  @metodos_redefinidos = []
-  define_singleton_method :method_added do |metodo_nuevo|
-    unless @metodos_redefinidos.include? metodo_nuevo
-      @metodos_redefinidos.append metodo_nuevo
-      sym_aux_metodos = "#{metodo_nuevo}_aux".to_sym
-      @metodos_redefinidos.append sym_aux_metodos
-      alias_method(sym_aux_metodos, metodo_nuevo)
-      define_method metodo_nuevo do ||
-        pre_cond.call
-        send(sym_aux_metodos)
-        post_cond.call
-      end
-    end
-  end
-end
-#hasta aca
+require './rubyapi'
 
 class MiClase
 
