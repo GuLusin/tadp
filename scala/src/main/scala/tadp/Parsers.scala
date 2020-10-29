d5757c4ae6842a7cd2e5bc51415f3c77e5b97653
GuLusin <44123833+GuLusin@users.noreply.github.com>
2020-10-29 16:47:58 -0300

    Generic Typing

--- scala/src/main/scala/tadp/Parsers.scala
+++ scala/src/main/scala/tadp/Parsers.scala
@@ -3,15 +3,15 @@ package tadp.parserCombinators
 import scala.util.{Failure, Success, Try}
 
 object ErrorDeParseo extends RuntimeException("No se pudo parsear")
-trait Parser extends (String => Try[(Any, String)]) {
-  
-  def <|>(parser: Parser): Parser = (stringRecibido: String) =>
+trait Parser[T] extends (String => Try[(T, String)]) {
+
+  def <|>(parser: Parser[T]): Parser[T] = (stringRecibido: String) =>
     this.apply(stringRecibido) match {
       case Success(tuple) => Success(tuple)
       case Failure(_) => parser.apply(stringRecibido)
     }
 
-  def <>(parser: Parser): Parser = (stringRecibido: String) =>
+  def <>[R](parser: Parser[R]): Parser[(T, R)] = (stringRecibido: String) =>
     this.apply(stringRecibido) match {
       case Success((parsedElement1, s2 : String)) =>
         parser.apply(s2) match {
@@ -21,13 +21,13 @@ trait Parser extends (String => Try[(Any, String)]) {
       case Failure(_) => Failure(ErrorDeParseo)
     }
 
-  def ~>(parser: Parser): Parser = (stringRecibido: String) =>
+  def ~>[R](parser: Parser[R]): Parser[R] = (stringRecibido: String) =>
     this.apply(stringRecibido) match {
       case Success((_, s2 : String)) => parser.apply(s2)
       case Failure(_) => Failure(ErrorDeParseo)
     }
 
-  def <~(parser: Parser): Parser = (stringRecibido: String) =>
+  def <~[R](parser: Parser[R]): Parser[T] = (stringRecibido: String) =>
     this.apply(stringRecibido) match {
       case Success((parsedElement1, s2 : String)) =>
         parser.apply(s2) match {
@@ -39,7 +39,7 @@ trait Parser extends (String => Try[(Any, String)]) {
 
   // FIXME:
   // en vez de Any => Any: quisiera que vaya de TipoQueDevuelveEsteParserEnSuccess => Any
-  def satisfies(f : Any => Boolean): Parser = (stringRecibido: String) =>
+  def satisfies(f : T => Boolean): Parser[T] = (stringRecibido: String) =>
     this.apply(stringRecibido) match {
       case Success((parsed, toParse)) => if(f(parsed)) Success((parsed, toParse)) else Failure(ErrorDeParseo)
       case Failure(_) => Failure(ErrorDeParseo)
@@ -51,49 +51,51 @@ trait Parser extends (String => Try[(Any, String)]) {
   //  precedencia("infija")  --->  Success(((null,fija),))
   //  esta bien?
 
-  def opt(): Parser = (stringRecibido: String) =>
+  def opt(): Parser[Option[T]] = (stringRecibido: String) =>
     this.apply(stringRecibido) match {
-      case Success(res) => Success(res)
-      case Failure(_) => Success((None, stringRecibido))
+      case Success((parsed, toParse : String)) => Success(Some(parsed), toParse)
+      case Failure(_) => Success(None, stringRecibido)
     }
 
   // La clausura de Kleene se aplica a un parser, convirtiéndolo en otro que se puede aplicar todas las veces que sea posible o 0 veces.
   // El resultado debería ser una lista que contiene todos los valores que hayan sido parseados (podría no haber ninguno).
-  def *(): Parser = (stringRecibido: String) =>
+  def *(): Parser[T] = (stringRecibido: String) =>
     ???
 
-  def +(): Parser = (stringRecibido: String) =>
+
+  def +(): Parser[T] = (stringRecibido: String) =>
     ???
 
   // FIXME:
   // en vez de Any => Any: quisiera que vaya de TipoQueDevuelveEsteParserEnSuccess => Any
-  def map(f : Any => Any): Parser = (stringRecibido: String) =>
+  def map[R](f : T => R): Parser[R] = (stringRecibido: String) =>
     this.apply(stringRecibido) match {
+        // probar con map posta
       case Success((parsedElement, str : String)) => Success(f(parsedElement),str)
       case Failure(_) => Failure(ErrorDeParseo)
     }
 }
 
-object anyChar extends Parser{
-  override def apply(stringRecibido: String): Try[(Char, String)] = Try(stringRecibido.charAt(0), stringRecibido.substring(1))
+object anyChar extends Parser[Char]{
+  override def apply(stringRecibido: String): Try[(Char, String)] = Try(stringRecibido.head, stringRecibido.substring(1))
 }
 
-case class char(_char : Char) extends Parser {
-  override def apply(stringRecibido: String): Try[(Char, String)] = if (stringRecibido.startsWith(_char.toString)){
+case class char(_char : Char) extends Parser[Char] {
+  override def apply(stringRecibido: String): Try[(Char, String)] = if (stringRecibido.head == _char){
     Success((_char, stringRecibido.substring(1)))
   }
   else
     Failure(ErrorDeParseo)
 }
 
-object digit extends Parser{
-  override def apply(stringRecibido: String): Try[(Char, String)] = if (stringRecibido.charAt(0).isDigit)
+object digit extends Parser[Char]{
+  override def apply(stringRecibido: String): Try[(Char, String)] = if (stringRecibido.head.isDigit)
     Success(stringRecibido.charAt(0), stringRecibido.substring(1))
   else
     Failure(ErrorDeParseo)
 }
 
-case class string(s1: String) extends Parser {
+case class string(s1: String) extends Parser[String] {
   override def apply(stringRecibido: String): Try[(String, String)] = if (stringRecibido.startsWith(s1))
     Success(s1, stringRecibido.substring(s1.length))
   else
