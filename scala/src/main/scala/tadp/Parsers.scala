34b48bb6c48fd0bc3a07204930d3a69d6ca42ef8
GuLusin <44123833+GuLusin@users.noreply.github.com>
2020-10-31 13:51:42 -0300

    Quedo todo mas lindo

--- scala/src/main/scala/tadp/Parsers.scala
+++ scala/src/main/scala/tadp/Parsers.scala
@@ -1,101 +1,75 @@
 package tadp.parserCombinators
 
-import scala.::
 import scala.util.{Failure, Success, Try}
 
-// TODO: 
-//  case Failure(_) => parser.apply(stringRecibido) está por todooos lados
+//  alternativas de tipado de Parser
+//  trait Parser[T] extends (String => Try[(T, String)])
+//  trait Parser[T] extends (String => Try[Resultado])
+//  trait Parser[T] extends (String => Resultado)
 
 object ErrorDeParseo extends RuntimeException("No se pudo parsear")
 trait Parser[T] extends (String => Try[(T, String)]) {
 
   def <|>(parser: Parser[T]): Parser[T] = (stringRecibido: String) =>
-    this.apply(stringRecibido) match {
-      case Success(tuple) => Success(tuple)
-      case Failure(_) => parser.apply(stringRecibido)
-    }
+    this.apply(stringRecibido) recoverWith { case ErrorDeParseo => parser.apply(stringRecibido) }
 
+  
   def <>[R](parser: Parser[R]): Parser[(T, R)] = (stringRecibido: String) =>
-    this.apply(stringRecibido) match {
-      case Success((parsedElement1, s2: String)) =>
-        parser.apply(s2) match {
-          case Success((parsedElement2, strToParse: String)) => Success((parsedElement1, parsedElement2), strToParse)
-          case Failure(_) => Failure(ErrorDeParseo)
-        }
-      case Failure(_) => Failure(ErrorDeParseo)
-    }
+    for {
+      (parsedElement1, s2) <- this.apply(stringRecibido)
+      (parsedElement2, strToParse: String) <- parser.apply(s2)
+    } yield ((parsedElement1, parsedElement2), strToParse)
 
+  
   def ~>[R](parser: Parser[R]): Parser[R] = (stringRecibido: String) =>
-    this.apply(stringRecibido) match {
-      case Success((_, s2: String)) => parser.apply(s2)
-      case Failure(_) => Failure(ErrorDeParseo)
-    }
+    this.apply(stringRecibido) flatMap { case (_, toParse: String) => parser.apply(toParse) }
+
 
   def <~[R](parser: Parser[R]): Parser[T] = (stringRecibido: String) =>
-    this.apply(stringRecibido) match {
-      case Success((parsedElement1, s2: String)) =>
-        parser.apply(s2) match {
-          case Success((_, strToParse: String)) => Success(parsedElement1, strToParse)
-          case Failure(_) => Failure(ErrorDeParseo)
-        }
-      case Failure(_) => Failure(ErrorDeParseo)
-    }
+    for {
+      (parsedElement1, s2) <- this.apply(stringRecibido)
+      (_, strToParse) <- parser.apply(s2)
+    } yield (parsedElement1, strToParse)
+
 
   def sepBy[R](separatorParser: Parser[R]): Parser[List[T]] = (stringRecibido: String) =>
-    this.<>((separatorParser.~>(this)).*())(stringRecibido) match {
-      case Success(((primerParsed,listaParsed),toParse)) => Success(( primerParsed :: listaParsed,toParse))
-      case Failure(_) => Failure(ErrorDeParseo)
-    }
+    this.<>(separatorParser.~>(this).*())(stringRecibido)
+      .map { case ((primerParsed, listaParsed), toParse) => (primerParsed :: listaParsed, toParse) }
 
-  def satisfies(f : T => Boolean): Parser[T] = (stringRecibido: String) =>
-    this.apply(stringRecibido) match {
-      case Success((parsed, toParse)) => if(f(parsed)) Success((parsed, toParse)) else Failure(ErrorDeParseo)
-      case Failure(_) => Failure(ErrorDeParseo)
-    }
 
-//  FIXME:
-//   esta bien option[T]? SI, MESSIRVE
+  def satisfies(f: T => Boolean): Parser[T] = (stringRecibido: String) =>
+    this.apply(stringRecibido) flatMap { case (parsed, toParse) => if (f(parsed)) Success((parsed, toParse)) else Failure(ErrorDeParseo) }
+
+
   def opt(): Parser[Option[T]] = (stringRecibido: String) =>
     this.apply(stringRecibido) match {
       case Success((parsed, toParse : String)) => Success(Some(parsed), toParse)
       case Failure(_) => Success(None, stringRecibido)
     }
 
-//  La clausura de Kleene se aplica a un parser, convirtiéndolo en otro que se puede aplicar todas las veces que sea posible o 0 veces.
-//  El resultado debería ser una lista que contiene todos los valores que hayan sido parseados (podría no haber ninguno).
-//  FIXME: anyChar.*() devuelve una lista de Char, estaria bueno que devuelva un String
+  
   def *(): Parser[List[T]] = (stringRecibido: String) =>
     this.apply(stringRecibido) match {
-      case Success((parsed, toParse : String)) =>
-        val (listaSiguienteIteracion, strSiguienteIteracion) : (List[T], String) = this.*().apply(toParse).get
-        if (listaSiguienteIteracion.isEmpty) Success(List(parsed), toParse) else Success(parsed :: listaSiguienteIteracion , strSiguienteIteracion)
+      case Success((parsed, toParse: String)) =>
+        val (listaSiguienteIteracion, strSiguienteIteracion): (List[T], String) = this.*().apply(toParse).get
+        if (listaSiguienteIteracion.isEmpty) Success(List(parsed), toParse) else Success(parsed :: listaSiguienteIteracion, strSiguienteIteracion)
       case Failure(_) => Success(List.empty[T], stringRecibido)
     }
 
 
-//  Es como la clausura de Kleene pero requiere que el parser se aplique al menos UNA vez.
   def +(): Parser[List[T]] = (stringRecibido: String) =>
-      this.*().apply(stringRecibido) match {
-        case Success((List(),_)) => Failure(ErrorDeParseo)
-        case Success(res) => Success(res)
-      }
-
-  def map[R](f : T => R): Parser[R] = (stringRecibido: String) =>
-    this.apply(stringRecibido) match {
-      case Success((parsedElement, str : String)) => Success(f(parsedElement),str)
-      case Failure(_) => Failure(ErrorDeParseo)
+    this.*().apply(stringRecibido) match {
+      case Success((List(), _)) => Failure(ErrorDeParseo)
+      case Success(res) => Success(res)
     }
-}
 
-//  TODO:
-//    En muchos lados hacemos:
-//    if condicion(strAEvaluar):
-//     Success(tuplaDeRetorno)
-//    else
-//     Failure(ErrorDeParseo)
-//
-//    podriamos usar una funcion privada tipo evaluar(condicion, tuplaDeRetorno)
-//    y evitar repeticion de codigo
+  
+  def map[R](f: T => R): Parser[R] = (stringRecibido: String) =>
+    //    this.apply(stringRecibido).flatMap { case (parsedElement, str : String) => Success(f(parsedElement),str)}
+    this.apply(stringRecibido).map { case (parsedElement, toParse) => (f(parsedElement), toParse) }
+
+  
+}
 
 object anyChar extends Parser[Char]{
   override def apply(stringRecibido: String): Try[(Char, String)] =
@@ -133,12 +107,12 @@ object integer extends Parser[Int] {
   override def apply(stringRecibido: String): Try[(Int, String)] = {
     val parserAuxiliar: Parser[(Option[Char], List[Char])] = char('-').opt() <> digit.+
     parserAuxiliar(stringRecibido) match {
-      case Success(((_: Some[Char], charlist: List[Char]), str: String)) => Success((-charlist.mkString.toInt, str))
-      case Success(((None, charlist: List[Char]), str: String)) => Success((charlist.mkString.toInt, str))
+      case Success(((opt : Option[Char], charList : List[Char]), str2 : String)) => Success((opt.getOrElse("") :: charList).mkString.toInt, str2)
       case Failure(_) => Failure(ErrorDeParseo)
     }
   }
 }
+
 
 object double extends Parser[Double] {
 
@@ -167,5 +141,3 @@ object double extends Parser[Double] {
 
 
 //---------------------------------------------------------------------
-
-
