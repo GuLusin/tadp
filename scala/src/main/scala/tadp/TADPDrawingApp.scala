e501938d8650246f4453e0cfb7989edfd69fa94d
GuLusin <guido.lusin@gmail.com>
2020-11-11 18:02:19 -0300

    terminamos el punto 2

--- scala/src/main/scala/tadp/TADPDrawingApp.scala
+++ scala/src/main/scala/tadp/TADPDrawingApp.scala
@@ -2,58 +2,15 @@ package tadp
 
 import scalafx.scene.paint.Color
 import tadp.internal.TADPDrawingAdapter
-import tadp.parserCombinators.anyChar.<|>
-import tadp.parserCombinators.{Parser, char, double, integer, string}
 
 object TADPDrawingApp extends App {
 
-
-
-  def parametros[T](parser :Parser[T]) : Parser[List[T]] = (stringRecibido :String) => {
-    (( char('[') ~> parser.sepBy(string(", ")) ) <~ char(']'))(stringRecibido)
-  }
-
-  def funcion[T](nombre :String, tipoParametro :Parser[T]) : Parser[List[T]] = (stringRecibido :String) => {
-    (string(nombre) ~> parametros(tipoParametro))(stringRecibido)
-  }
-
-
-  val escala = (string("escala") <> char('[')) ~> double <> ((string(", ") ~> double) <~ char(']'))
-
-  val escala2 = parametros(integer)
-
-  val punto = (integer <~ string(" @ ")) <> integer
-
-  val parDePuntos = parametros(punto)
-
-  val color = funcion("color",integer)
-
-  val grupo = funcion("grupo",color)
-
-  val prueba = char('c') <|> char('h')
-
-  println(prueba("caca"))
-  println(prueba("hola"))
-  println(prueba("perro"))
-
-  println(escala2("[45, 45, 415, 1000]"))
-
-  println( parDePuntos("[0 @ 0, -400 @ 400]"))
-
-
-
-  println(escala("escala[1.45, 1.45]"))
-
-  println(punto("45 @ 45"))
-
-
-
-  // Ejemplo de uso del drawing adapter:
-  //  TADPDrawingAdapter
-  //    .forScreen { adapter =>
-  //      adapter
-  //        .beginColor(Color.rgb(100, 100, 100))
-  //        .rectangle((200, 200), (400, 400))
-  //        .end()
-  //    }
+//   Ejemplo de uso del drawing adapter:
+    TADPDrawingAdapter
+      .forScreen { adapter =>
+        adapter
+          .beginColor(Color.rgb(100, 100, 100))
+          .rectangle((200, 200), (400, 400))
+          .end()
+      }
 }
--- scala/src/main/scala/tadp/parserCombinators/domain.scala
+++ scala/src/main/scala/tadp/parserCombinators/domain.scala
@@ -1,25 +1,31 @@
 package tadp.parserCombinators
-
-import scala.io.Source
+import scala.annotation.tailrec
+import scala.util.Try
 
 trait Elemento
 
 case class Punto(x : Double, y : Double) extends Elemento
 
-case class Triangulo(p1 : Punto, p2 : Punto, p3 : Punto) extends Elemento
 
-case class Rectangulo(supIzq : Punto, infDer : Punto) extends Elemento
+trait Figura extends Elemento
 
-case class Circulo(centro : Punto, radio : Double) extends Elemento
+case class Triangulo(p1 : Punto, p2 : Punto, p3 : Punto) extends Figura
+
+case class Rectangulo(supIzq : Punto, infDer : Punto) extends Figura
+
+case class Circulo(centro : Punto, radio : Double) extends Figura
+
 
 case class Grupo(elementos : List[Elemento]) extends Elemento
 
 case class TokenFinGrupo() extends Elemento
 
+
+
 //case aca rompe
 class Transformador(aplicaSobre : Elemento) extends Elemento
 
-case class color(rojo : Int, verde : Int, azul : Int, _aplicaSobre : Elemento) extends Transformador(_aplicaSobre){
+case class Color(rojo : Int, verde : Int, azul : Int, _aplicaSobre : Elemento) extends Transformador(_aplicaSobre){
   require( 0 <= rojo  && rojo  <= 255 , "Codigo de color debe estar entre 0 y 255" )
   require( 0 <= verde && verde <= 255 , "Codigo de color debe estar entre 0 y 255" )
   require( 0 <= azul  && azul  <= 255 , "Codigo de color debe estar entre 0 y 255" )
@@ -36,45 +42,76 @@ case class Translacion(desX : Double, desY : Double, _aplicaSobre : Elemento) ex
 
 object polenta extends App{
 
+  val source = scala.io.Source.fromFile("C:\\Users\\guido\\IdeaProjects\\grupo10-2020-2c\\scala\\src\\main\\scala\\tadp\\parserCombinators\\asd.dibujitos")
+  var str = source.getLines mkString "\n"
 
-  val dotPraser : Parser[Punto] = (double <~ char(' ').opt() <~ char('@') <~ char(' ').opt <> double).map { case (x, y) => Punto(x, y) }
-//
-  val recParser : Parser[Rectangulo] = (string("rectangulo[") ~> dotPraser <~ string(", ") <> dotPraser <~ char(']')) map { case (x, y) => Rectangulo(x, y) }
-  val triParser : Parser[Triangulo] = (string("triangulo[") ~> dotPraser <~ string(", ") <> dotPraser <~ string(", ") <> dotPraser <~ char(']')) map { case ((x, y), z) => Triangulo(x, y, z) }
-  val cirParser : Parser[Circulo] = (string("circulo[") ~> dotPraser <~ string(", ") <> double <~ char(']')) map { case (centro, radio) => Circulo(centro, radio) }
+  source.close()
 
 
-  val grupoParser : Parser[Grupo] = (string("grupo(") ~> ( recParser <|> triParser <|> cirParser <|> grupoParser ).sepBy(char(',')) <~ char(')')) map { case l : List[Elemento] => Grupo(l) }
+  str = str.replaceAll("\\s","")
+
+//  println(str)
+
+  val coma : Parser[Char] = char(',')
+
+  val dotPraser : Parser[Punto] = (double <~ char('@') <> double).map { case (x, y) => Punto(x, y) }
+
+  val recParser : Parser[Rectangulo] = (string("rectangulo[") ~> dotPraser <~ coma <> dotPraser <~ char(']')) map { case (x, y) => Rectangulo(x, y) }
+  val triParser : Parser[Triangulo] = (string("triangulo[") ~> dotPraser <~ coma <> dotPraser <~ coma <> dotPraser <~ char(']')) map { case ((x, y), z) => Triangulo(x, y, z) }
+  val cirParser : Parser[Circulo] = (string("circulo[") ~> dotPraser <~ coma <> double <~ char(']')) map { case (centro, radio) => Circulo(centro, radio) }
+
+  val colorParser : Parser[Color] = (string("color[") ~> integer <~ coma <> integer <~ coma <> integer <~ string("](") <> elemParser <~ char(')'))
+    .map {case (((r : Int, g : Int), b: Int), elemento: Elemento) => Color(r, g, b, elemento) }
+
+  val escalaParser : Parser[Escala] = (string("escala[") ~> double <~ coma <> double <~ string("](") <> elemParser <~ char(')'))
+    .map {case ((x: Double, y: Double), elemento: Elemento) => Escala(x, y, elemento) }
+
+  val rotParser : Parser[Rotacion] = (string("rotacion[") ~> integer <~ string("](") <> elemParser <~ char(')'))
+    .map {case (angulo : Int, elemento: Elemento) => Rotacion(angulo % 360, elemento) }
+
+  val translacionParser : Parser[Translacion] = (string("translacion[") ~> double <~ coma <> double <~ string("](") <> elemParser <~ char(')'))
+    .map {case ((x: Double, y: Double), elemento: Elemento) => Translacion(x, y, elemento) }
+
+  def transformadorParser : Parser[Transformador] = colorParser <|> escalaParser <|> rotParser <|> translacionParser
+
+  def figuraParser : Parser[Figura] = recParser <|> triParser <|> cirParser
+
+  def elemParser : Parser[Elemento] = figuraParser <|> grupoParser <|> transformadorParser
+
+  def grupoParser : Parser[Grupo] = (string("grupo(") ~> elemParser.sepBy(coma) <~ char(')')) map { l: List[Elemento] => Grupo(l) }
 
 
-//  val res = grupoParser("grupo(circulo[15 @ 147, 651],circulo[420 @ 147, 666])")
-//  val res2 = grupoParser("grupo(circulo[15 @ 147, 651])")
-  val res3 = grupoParser("grupo(grupo(rectangulo[186@ 840, 400@150]))")
-//
-//  println(res)
-//  println(res2)
-//  println(res3)
+  val aux = elemParser(str)
 
-//  val res = grupoParser("circulo[15 @ 147, 651]")
+  println(aux)
 
-  println(res3)
+  if(aux.isSuccess){
+    println("Sin simplificar:")
+    println(aux.get._1)
+  }else{
+    println("rompio todo, pa")
+    println(aux)
+  }
+
+  @tailrec
+  def simplificador(elemento: Elemento) : Elemento = {
+    elemento match {
+      case figura: Figura => figura
+      case Grupo(elemento :: Nil) => simplificador(elemento)
+//      case Grupo(elementos : List[Elemento]) => Grupo(simplificador(elementos))
+      case a => a
+    }
+  }
 
 
+  if(aux.isSuccess){
+    println("Simplificado:")
+    println(simplificador(aux.get._1))
+  }else{
+    println("rompio todo, pa")
+    println(aux)
 
-  //  val parsersPosibles : List[Parser[ _ <: Elemento ]] = List(recParser, triParser, cirParser)
-//
-//  def sentences(unStr : String): List[Elemento] = {
-//    val unParser = parsersPosibles.find(_.apply(unStr).isSuccess)
-//
-//    unParser.get.apply(unStr) match {
-//      case Success((parsed, "")) => List(parsed)
-//      case Success((parsed, toParse)) => parsed :: sentences(toParse)
-//      case Failure(_) => List()
-//    }
-//
-//
-//  }
-//
-//  println(sentences(fileContents))
+  }
+
 
 }
